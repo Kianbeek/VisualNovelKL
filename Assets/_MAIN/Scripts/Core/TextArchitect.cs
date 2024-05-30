97269b458475f65ef05e10371b6619e19f568bc3
Kianbeek <kianscoort@yahoo.nl>
2024-05-30 16:50:30 +0200

    Update TextArchitect

--- Assets/_MAIN/Scripts/Core/TextArchitect.cs
+++ Assets/_MAIN/Scripts/Core/TextArchitect.cs
@@ -1,19 +1,79 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class TextArchitect : MonoBehaviour
+public class TextArchitect
 {
-    // Start is called before the first frame update
-    void Start()
+    private TextMeshProUGUI tmpro_ui;
+    private TextMeshPro tmpro_world;
+    public TMP_Text tmpro => tmpro_ui != null ? tmpro_ui : tmpro_world;
+
+    public string currentText => tmpro.text;
+    public string targetText { get; private set; } = "";
+    public string preText { get; private set; } = "";
+    private int preTextLength = 0;
+
+    public string fullTargetText => preText + targetText;
+
+    public enum BuildMethod { instant, typewriter, fade}
+    public BuildMethod buildMethod = BuildMethod.typewriter;
+    public Color textColor { get { return tmpro.color; } set { tmpro.color = value; } }
+
+    public float speed { get { return baseSpeed * speedMultiplier; } set { speedMultiplier = value; } }
+    private const float baseSpeed = 1;
+    private float speedMultiplier = 1;
+
+    public int charactersPerCycle { get { return speed <= 2f ? characterMultiplier : speed <= 2.5f ? characterMultiplier * 2 : characterMultiplier * 3; } }
+    private int characterMultiplier = 1;
+
+    public bool hurryUp = false;
+
+    public TextArchitect(TextMeshProUGUI tmpro_ui)
     {
-        
+        this.tmpro_ui = tmpro_ui;
     }
 
-    // Update is called once per frame
-    void Update()
+    public TextArchitect(TextMeshProUGUI tmpro_world)
     {
-        
+        this.tmpro_world = tmpro_world;
+    }
+
+    public Coroutine Build(string text)
+    {
+        preText = "";
+        targetText = text;
+
+        Stop();
+
+        buildProcess = tmpro.StartCoroutine(Building());
+        return buildProcess;
+    }
+
+    public Coroutine Append(string text)
+    {
+        preText = tmpro.text;
+        targetText = text;
+
+        Stop();
+
+        buildProcess = tmpro.StartCoroutine(Building());
+        return buildProcess;
+    }
+
+    private Coroutine buildProcess = null;
+    public bool isBuilding => buildProcess != null;
+
+    public void Stop()
+    {
+        if (!isBuilding)
+            return;
+
+        tmpro.StopCoroutine(buildProcess); 
+        buildProcess = null;
+    }
+
+    IEnumerator Building()
+    {
+        yield return null;
     }
 }
